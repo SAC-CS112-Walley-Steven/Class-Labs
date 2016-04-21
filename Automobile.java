e37cb3289b760b626f14089b916813dc98a256b7
Steven Walley <swalley2626@yahoo.com>
2016-04-20 21:55:59 -0700

    Added files via upload

--- Automobile.java
+++ Automobile.java
@@ -15,10 +15,31 @@ public class Automobile {
 		this.color = color;
 		this.name = name;
 	}
-
+	public void setDoors(int doors)
+	{
+		this.doors = doors;
+	}
+	public void setCylinder(int cylinder)
+	{
+		this.doors = doors;
+	}
+	public void setTrans(int trans)
+	{
+		this.transmission = trans;
+	}
+	public void setColor (String color)
+	{
+		this.color = color;
+	}
+	public void setName (String name)
+	{
+		this.name = name;
+	}
+	
 	public String toString()
 	{
 		return String.format("\nThis automobile has:\n%d doors\n%d cylinders\n%d speed transmition\n%s in color\n%s is the name",
 				doors, cylinders, transmission, color, name);
 	}
+	
 }
