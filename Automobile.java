1c6cb056cfd1a99ec1aefe99d5b7ac744d985724
Steven Walley <swalley2626@yahoo.com>
2016-04-20 21:48:38 -0700

    Added files via upload

--- Automobile.java
+++ Automobile.java
@@ -1,4 +1,24 @@
 
 public class Automobile {
+	
+	private int doors;
+	private int cylinders;
+	private int transmission;
+	private String color;
+	private String name;
+	
+	public Automobile (int doors, int cylinder, int trans, String color, String name)
+	{
+		this.doors=doors;
+		this.cylinders = cylinder;
+		transmission = trans;
+		this.color = color;
+		this.name = name;
+	}
 
+	public String toString()
+	{
+		return String.format("\nThis automobile has:\n%d doors\n%d cylinders\n%d speed transmition\n%s in color\n%s is the name",
+				doors, cylinders, transmission, color, name);
+	}
 }
--- AutomobileTest.java
+++ AutomobileTest.java
@@ -2,8 +2,15 @@
 public class AutomobileTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Automobile automobile1 = new Automobile (4,6,5, "Yellow", "Cabbie");
+		System.out.println(automobile1);
+		
+		Automobile automobile2 = new Automobile (2,6,5, "Red", "Sporty");
+		System.out.println(automobile2);
+		
+		Automobile automobile3 = new Automobile (5,6,5, "Green", "Vany");
+		System.out.println(automobile3);
 	}
 
 }
